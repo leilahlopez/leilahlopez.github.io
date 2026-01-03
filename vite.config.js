6be4e2d83bdd746bccece929eb9af30db517b254
leilahsmith <leilah.sdev@gmail.com>
2026-01-02 19:53:01 -0500

    VITE CONFIG UPDATE

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,11 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from "@tailwindcss/vite";
 import path from 'path';
 import svgr from "vite-plugin-svgr";
+import { fileURLToPath } from 'url'
+
+// ✅ ESM-safe replacements for __dirname
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 // https://vite.dev/config/
 export default defineConfig({
